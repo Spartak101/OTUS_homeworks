0027ea5808e2dc0f1bd87c6006836f5a4589ccd3
Spartak Iskhakov <spartak10182@yandex.ru>
2025-07-13 23:22:55 +0300

    Initial commit

--- untitled/src/main/java/org/example/DiscriminantSearch.java
+++ untitled/src/main/java/org/example/DiscriminantSearch.java
@@ -4,18 +4,24 @@ public class DiscriminantSearch implements Discriminant {
     double a;
     double b;
     double c;
+    double epsilon;
 
-    public DiscriminantSearch(double a, double b, double c) {
-        try {
-            if(a < Double.MIN_VALUE){
-                throw new Exception("а не может быть равен нулю");
-            } else
+
+    public DiscriminantSearch(double a, double b, double c, double epsilon) {
+//        try {
+            Double doubleValue = Double.valueOf(a);
+            Double doubleEpsilon = Double.valueOf(0.0);
+            if (doubleValue.compareTo(doubleEpsilon) == 0) {
+                throw new IllegalArgumentException("а не может быть равен нулю");
+            } else {
                 this.a = a;
-            this.b = b;
-            this.c = c;
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+                this.b = b;
+                this.c = c;
+                this.epsilon = epsilon;
+            }
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
     }
 
     public double getA() {
@@ -30,21 +36,73 @@ public class DiscriminantSearch implements Discriminant {
         return c;
     }
 
+    public double getEpsilon() {
+        return epsilon;
+    }
+
     @Override
     public void discriminant() {
         System.out.println("discriminant");
     }
 
-    public double[] discriminantSearch(double a, double b, double c) {
+    public double[] discriminantSearch(double a, double b, double c, double epsilon) {
         double[] doubles;
         double d = (b * b - 4 * a * c);
-        if (d > 0) {
-            doubles = new double[]{(-b+Math.sqrt(d))/(2*a),(-b-Math.sqrt(d))/(2*a)};
-        } else if (d < 0){
+        if (d > 0 && d >= epsilon) {
+            doubles = new double[]{(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)};
+            return doubles;
+        } else if (d < 0 && Math.abs(d) >= epsilon) {
             doubles = new double[]{};
-        } else {
-            doubles = new double[]{(-b)/(2*a),(-b)/(2*a)};
+            return doubles;
+        } else if(Math.abs(d) < epsilon){
+            doubles = new double[]{(-b) / (2 * a), (-b) / (2 * a)};
+            return doubles;
         }
-        return doubles;
+        return null;
+    }
+
+    private boolean checkingTheBoundaryValues(double a, double b, double c) throws Exception{
+        Double aa = Double.valueOf(Math.abs(a));
+        Double bb = Double.valueOf(Math.abs(b));
+        Double cc = Double.valueOf(Math.abs(c));
+        Double doubleValueZero = Double.valueOf(0.0);
+        Double doubleMinValue = Double.MIN_VALUE;
+        if(aa.compareTo(doubleValueZero) == 0){
+            throw new IllegalArgumentException("а не может быть равен нулю");
+        } else if (aa.compareTo(doubleMinValue) == -1) {
+            throw new IllegalArgumentException("а не может быть меньше минимального значения для double");
+        } else if (bb.compareTo(doubleMinValue) == -1) {
+            throw new IllegalArgumentException("b не может быть меньше минимального значения для double");
+        } else if (cc.compareTo(doubleMinValue) == -1) {
+            throw new IllegalArgumentException("c не может быть меньше минимального значения для double");
+        }
+        return true;
+    }
+
+    private boolean TheAValueIsTooHigh(double a, String string) throws IllegalArgumentException {
+        Double aa = Double.valueOf(Math.abs(a));
+        Double doubleMaxValue = Double.MAX_VALUE;
+        if(aa.compareTo(doubleMaxValue) == 1){
+            throw new IllegalArgumentException(string + " не может быть больше максимального значения для double");
+        }
+        return true;
+    }
+
+
+
+    private boolean TheAValueIsTooSmall(double a, String string){
+        Double aa= Double.valueOf(Math.abs(a));
+        Double doubleMinValue = Double.MIN_VALUE;
+        if(aa.compareTo(doubleMinValue) == -1){
+            throw new IllegalArgumentException(string + " не может быть меньше минимального значения для double");
+        }
+        return true;
+    }
+
+    private boolean checkingForNaN(double a, String string){
+         if(!Double.isNaN(a)){
+            throw new IllegalArgumentException(string + " не является числом");
+        }
+        return true;
     }
 }
--- untitled/src/test/java/DiscriminantSearchTest.java
+++ untitled/src/test/java/DiscriminantSearchTest.java
@@ -1,25 +1,75 @@
 import org.example.DiscriminantSearch;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @DisplayName("Тест класса DiscriminantSearch ")
 public class DiscriminantSearchTest {
+    double epsilon = 0.0000000001;
 
     @Test
-    @DisplayName("b равен нулю")
-    public void bIsEqualToZero() {
+    @DisplayName("Дискриминант меньше нулю")
+    public void discriminantIsLessThanZero() {
         double a = 1.0;
         double b = 0.0;
         double c = 1.0;
-        DiscriminantSearch discriminantSearch = new DiscriminantSearch(a, b, c);
+        DiscriminantSearch discriminantSearch = new DiscriminantSearch(a, b, c, epsilon);
 
         double[] result = discriminantSearch.discriminantSearch(discriminantSearch.getA(),
                                                                 discriminantSearch.getB(),
-                                                                discriminantSearch.getC());
+                                                                discriminantSearch.getC(),
+                                                                discriminantSearch.getEpsilon());
         double[] expected = new double[]{};
 
         assertArrayEquals(expected, result, 0.001); // 0.001 - допустимая погрешность
+    }
+
+    @Test
+    @DisplayName("Дискриминант больше нуля")
+    public void discriminantIsGreaterThanZero() {
+        double a = 1.0;
+        double b = 0.0;
+        double c = -1.0;
+        double d = (b * b - 4 * a * c);
+        DiscriminantSearch discriminantSearch = new DiscriminantSearch(a, b, c, epsilon);
+
+        double[] result = discriminantSearch.discriminantSearch(discriminantSearch.getA(),
+                                                                discriminantSearch.getB(),
+                                                                discriminantSearch.getC(),
+                                                                discriminantSearch.getEpsilon());
+        double[] expected = new double[]{(-b+Math.sqrt(d))/(2*a),(-b-Math.sqrt(d))/(2*a)};
+
+        assertArrayEquals(expected, result, 0.001); // 0.001 - допустимая погрешность
+    }
+
+    @Test
+    @DisplayName("Дискриминант равен нулю")
+    public void discriminantIsZero() {
+        double a = 1.0;
+        double b = 2.0;
+        double c = 0.99999999998;
+        DiscriminantSearch discriminantSearch = new DiscriminantSearch(a, b, c, epsilon);
+
+        double[] result = discriminantSearch.discriminantSearch(discriminantSearch.getA(),
+                                                                discriminantSearch.getB(),
+                                                                discriminantSearch.getC(),
+                                                                discriminantSearch.getEpsilon());
+        double[] expected = new double[]{(-b)/(2*a),(-b)/(2*a)};
+
+        assertArrayEquals(expected, result, 0.001); // 0.001 - допустимая погрешность
+    }
+
+    @Test
+    @DisplayName("а не равен нулю")
+    public void aIsNotEqualToZero() {
+        double a = 0.0;
+        double b = 1.0;
+        double c = 1.0;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new DiscriminantSearch(a, b, c, epsilon);
+        });
     }
 
 }
